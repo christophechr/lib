877756fce9fde9043408efb1431e807a927c216c
Christophe CHHOR <christophe.chhor@epitech.eu>
2021-12-10 14:50:38 +0100

    update: add working my_getnbr

--- my_getnbr.c
+++ my_getnbr.c
@@ -7,5 +7,20 @@
 
 int my_getnbr(char const *str)
 {
-    return 0;
+    int nbr = 0;
+    int neg_int = 1;
+    int i = 0;
+
+    for (; str[i] == '+' || str[i] == '-'; i++) {
+        if (str[i] == '-')
+            neg_int *= -1;
+    }
+    for (; str[i] != '\0'; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            nbr *= 10;
+            nbr = nbr + str[i] - '0';
+        } else
+            break;
+    }
+    return (nbr * neg_int);
 }
