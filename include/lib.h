bfeeae0665bb8578767851ce306a20831f502211
chris <christophe.chhor@epitech.eu>
2022-03-28 16:09:29 +0200

    update lib and remove no used functions

--- include/lib.h
+++ include/lib.h
@@ -20,51 +20,15 @@
 
     int my_getnbr(char const *);
 
-    void my_sort_int_array(int *, int);
-
-    int my_is_prime(int);
-
     char *my_strcpy(char *, char const *);
 
     char *my_strncpy(char *, char const *, int);
 
     char *my_revstr(char *);
 
-    int lenght_string_rev(char *);
-
     int my_strcmp(char const *, char const *);
 
     int my_strncmp(char const *, char const *);
-
-    char *my_strupcase(char *);
-
-    char *my_strlowcase(char *);
-
-    char *my_strcapitalize(char const *);
-
-    int compare_string_ascii(char const *, int);
-
-    int compare_string_ascii_num(char const *, int);
-
-    int my_str_isalpha(char const *);
-
-    int my_str_isnum(char const *);
-
-    int my_str_islower(char const *);
-
-    int my_str_isupper(char const *);
-
-    int my_str_isprintable(char const *);
-
-    int my_showstr(char const *);
-
-    int my_showmem(char const *, int);
-
-    int lenght_string_cat(char const *);
-
-    int lenght_string_b(char const *);
-
-    int lenght_string_ncat(char *);
 
     char *my_strcat(char *, char const *);
 
